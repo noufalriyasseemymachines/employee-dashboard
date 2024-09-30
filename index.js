b52cb928e7fce8f4d6ab131431d1dc1d3c22ddea
noufalriyasm <noufal.riyas@seemymachines.com>
2024-09-30 12:35:33 +0530

    updated

--- index.js
+++ index.js
@@ -1,46 +1,95 @@
-const employeeApi ="https://6580190d6ae0629a3f54561f.mockapi.io/api/v1/employee";
+const employeeApi = "https://6580190d6ae0629a3f54561f.mockapi.io/api/v1/employee";
 
-const errorMessage = document.getElementById("error-message");
 const addButton = document.getElementById("add-button");
 const modal = document.getElementById("modal");
 const closeButton = document.getElementById("close-modal");
 const filterDropdown = document.getElementById("filter-type");
 const filterInput = document.getElementById("filter-input");
+const snackbar = document.getElementById("snackbar"); 
+const submitButton=document.getElementById("submit-btn")
+const confirmationModal = document.getElementById("confirmation-modal");
+const confirmYesButton = document.getElementById("confirm-yes");
+const confirmNoButton = document.getElementById("confirm-no");
+const logoutButton=document.getElementById("logout-button")
+
+window.onload=checkLogin
+
+const loginSuccess=localStorage.getItem("isLoginSuccess");
+if(loginSuccess==="true"){
+    displaySnackbar("Logged Succesfully",true)
+    localStorage.removeItem("isLoginSuccess")
+}
+
+function checkLogin(){
+    const isLoggedIn=getCookie("isLoggedIn")
+    if(isLoggedIn!=="true"){
+        window.location.href="register.html"
+        displaySnackbar("Login and Continue",false)
+    }
+}
+
+function getCookie(name){
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const cookieArr = decodedCookie.split(';');
+    for (let i = 0; i < cookieArr.length; i++) {
+        let cookie = cookieArr[i].trim();
+        if (cookie.indexOf(name + "=") == 0) {
+            return cookie.substring(name.length + 1);
+        }
+    }
+    return "";
+}
+
+function setCookie(name,value,days){
+    const date=new Date()
+    date.setTime(date.getTime()+(days*24*60*60*1000))
+    const expires=`expires=${date.toUTCString()}`
+    document.cookie=`${name}=${value};${expires};path=/`
+}
+
+logoutButton.addEventListener("click",function(){
+    setCookie("isLoggedIn","false",0)
+    window.location.href="register.html"
+})
 
 addButton.addEventListener("click", function () {
-    modal.style.display = "flex";
+    document.getElementById("employee-id").value=""
+    document.getElementById("name").value=""
+    document.getElementById("age").value=""
+    document.getElementById("designation").value=""
+    modal.style.display = "flex"
+    submitButton.style.display="block"
 });
 
 closeButton.addEventListener("click", function () {
     modal.style.display = "none";
+    document.getElementById("employee-form").reset();
+
 });
 
 document.getElementById("employee-form").addEventListener("submit", function (event) {
-        event.preventDefault();
-        const id = document.getElementById("employee-id").value;
-        const name = document.getElementById("name").value;
-        const age = document.getElementById("age").value;
-        const designation = document.getElementById("designation").value;
-
-        const namePattern = /^[A-Za-z\s]+$/;
-        if (!namePattern.test(name)) {
-            displayError("Name can only contain letters and spaces.");
-            setTimeout(() => displayError(""), 1500);
-            this.reset();
+    event.preventDefault();
+    const id = document.getElementById("employee-id").value;
+    const name = document.getElementById("name").value;
+    const age = document.getElementById("age").value;
+    const designation = document.getElementById("designation").value;
+    const namePattern = /^[A-Za-z\s]+$/;
+    if (!namePattern.test(name)) {
+        displaySnackbar("Name can only contain letters and spaces.", false);
+        this.reset();
+    } else {
+        if (id) {
+            editEmployee(id, { name, age, designation });
         } else {
-            if (id) {
-                editEmployee(id, { name, age, designation });
-            } else {
-                addEmployee({ name, age, designation });
-            }
-            this.reset();
-            modal.style.display = "none";
+            addEmployee({ name, age, designation });
         }
-    });
+        this.reset();
+        modal.style.display = "none";
+    }
+});
 
 filterInput.addEventListener("input", fetchApi);
-filterDropdown.addEventListener("change",fetchApi);
-
+filterDropdown.addEventListener("change", fetchApi);
 async function fetchApi() {
     try {
         const response = await fetch(employeeApi);
@@ -51,43 +100,41 @@ async function fetchApi() {
 
         const filterType = filterDropdown.value;
         const filterValue = filterInput.value.toLowerCase();
-        const filteredData= ((filterType==="" && filterValue==="")||(filterType==="" && filterValue!==""))? data :
-        data.filter(employe =>{
-            return (filterType==="name" && employe.name.toLowerCase().includes(filterValue))||
-            (filterType==="designation" && employe.designation.toLowerCase().includes(filterValue))
-        })
-        if(filteredData.length>0){
-        displayEmployee(filteredData)
-        displayError("")
+        const filteredData = ((filterType === "" && filterValue === "") || (filterType === "" && filterValue !== "") || (filterType==="name" && filterValue==="") || (filterType==="designation" && filterValue===""))  ? data :
+            data.filter(employe => {
+                return (filterType === "name" && employe.name.toLowerCase().startsWith(filterValue)) ||
+                    (filterType === "designation" && employe.designation.toLowerCase().startsWith(filterValue));
+            });
+        if (filteredData.length > 0) {
+            displayEmployee(filteredData);
+        } else {
+            displayEmployee([])
+            displaySnackbar("Search result Not Found", false);
         }
-        else{
-        displayError("Search result Not Found")
-        displayEmployee(filteredData)
-        }
-    }
-    catch (error) {
-        displayError(error.message);
-        setTimeout(() => {
-            displayError(""), 1500;
-        });
+    } catch (error) {
+        displaySnackbar(error.message, false);
     }
 }
 
-function displayError(error, isSuccess = false) {
-    errorMessage.textContent = error;
-    if (isSuccess) {
-        errorMessage.style.color = "green";
-    } else {
-        errorMessage.style.color = "red";
-    }
+function displaySnackbar(message, isSuccess = false) {
+    snackbar.textContent = message;
+    snackbar.className = isSuccess ? "show success" : "show error";
+    snackbar.style.visibility = "visible";
+    snackbar.style.opacity = "1";
+    setTimeout(() => {
+        snackbar.className = "";
+        snackbar.style.visibility = "hidden";
+        snackbar.style.opacity = "0"; 
+    }, 2000);
 }
 
 function displayEmployee(employeData) {
     const employeeList = document.getElementById("employee-list");
     employeeList.innerHTML = "";
-    const table = document.createElement("table");
-    const thead = document.createElement("thead");
-    thead.innerHTML = `
+    if(employeData.length>0){
+        const table = document.createElement("table");
+        const thead = document.createElement("thead");
+        thead.innerHTML = `
         <tr>
             <th>Name</th>
             <th>Age</th>
@@ -95,10 +142,10 @@ function displayEmployee(employeData) {
             <th>Edit</th>
             <th>Delete</th>
         </tr>
-    `;
-    table.appendChild(thead);
-    const tbody = document.createElement("tbody");
-    employeData.forEach((employ) => {
+            `;
+        table.appendChild(thead);
+        const tbody = document.createElement("tbody");
+        employeData.forEach((employ) => {
         const row = document.createElement("tr");
         row.innerHTML = `
             <td data-label="Name">${employ.name}</td>
@@ -108,22 +155,66 @@ function displayEmployee(employeData) {
                 <button class="edit-btn" onclick="updateForm('${employ.id}', '${employ.name}', '${employ.age}', '${employ.designation}')">Edit</button>
             </td>
             <td data-label="Delete">
-                <button class="delete-btn" onclick="deleteEmploye('${employ.id}')">Delete</button>
+                <button class="delete-btn" onclick="showDeleteMessage('${employ.id}')">Delete</button>
             </td>
         `;
         tbody.appendChild(row);
-    });
+        });
 
-    table.appendChild(tbody);
-    employeeList.appendChild(table);
+        table.appendChild(tbody);
+        employeeList.appendChild(table);
+    }
+    else{
+        const noDataToShow=document.createElement("p")
+        noDataToShow.textContent=""
+        employeeList.appendChild(noDataToShow)
+    }
 }
 
+let deleteEmployeId=null
+function showDeleteMessage(id){
+    deleteEmployeId=id
+    confirmationModal.style.display="flex"
+}
+
+confirmYesButton.addEventListener("click",async function(){
+    confirmationModal.style.display="none"
+    await deleteEmploye(deleteEmployeId)
+})
+
+confirmNoButton.addEventListener("click",function(){
+    confirmationModal.style.display="none"
+})
+
+let originalUserData={}
 function updateForm(id, name, age, designation) {
     document.getElementById("employee-id").value = id;
     document.getElementById("name").value = name;
     document.getElementById("age").value = age;
     document.getElementById("designation").value = designation;
-    modal.style.display = "flex";
+
+    originalUserData={id,name,age,designation}
+    modal.style.display="flex"
+    checkChanges()
+}
+
+const formInput=document.querySelectorAll("#employee-form input");
+formInput.forEach(input=>{
+    input.addEventListener("input",checkChanges)
+})
+
+function checkChanges(){
+    const id=document.getElementById("employee-id").value
+    const name=document.getElementById("name").value
+    const age=document.getElementById("age").value
+    const designation=document.getElementById("designation").value
+    const isChanged=name !==originalUserData.name || age !==originalUserData.age || designation !==originalUserData.designation;
+    if(!isChanged){
+        submitButton.style.display="none"
+    }
+    else{
+        submitButton.style.display="block"
+    }
 }
 
 async function addEmployee(details) {
@@ -138,13 +229,10 @@ async function addEmployee(details) {
         if (!response.ok) {
             throw new Error("Sorry...There is an error Occured");
         }
-        
         await fetchApi();
-        displayError("Employee added succesfully", true);
-        setTimeout(() => displayError(""), 1500);
+        displaySnackbar("Employee added successfully", true);
     } catch (err) {
-        displayError(err.message);
-        setTimeout(() => displayError(""), 1500);
+        displaySnackbar(err.message, false);
     }
 }
 
@@ -161,13 +249,11 @@ async function editEmployee(id, newData) {
             throw new Error("Sorry...There is an error Occured");
         } else {
             await fetchApi();
-            displayError("Details Edited Succesfully", true);
-            setTimeout(() => displayError(""), 1500);
+            displaySnackbar("Details Edited Successfully", true);
             document.getElementById("employee-id").value = "";
         }
     } catch (err) {
-        displayError(err.message);
-        setTimeout(() => displayError(""), 1500);
+        displaySnackbar(err.message, false);
     }
 }
 
@@ -179,12 +265,11 @@ async function deleteEmploye(id) {
         if (!response.ok) {
             throw new Error("Sorry...There is an error Occured");
         }
-        displayError("Data deleted Succesfully",true)
-        setTimeout(()=>displayError(""),1500)
+        displaySnackbar("Data deleted Successfully", true);
+
         await fetchApi();
     } catch (err) {
-        displayError(err.message);
-        setTimeout(() => displayError(""), 1500);
+        displaySnackbar(err.message, false);
     }
 }
 fetchApi();
